078b78c1cce897b83b5c63552218d090e91234be
Houdini <albertbuluma76@gmail.com>
2026-04-15 19:21:04 +0300

    Python Multithreading

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import pandas as pd
-import requests
 import io
 import pytz
+import pandas as pd
+import requests
 from datetime import datetime
+import threading
 
 
 
@@ -25,12 +26,6 @@ def get_sp500_tickers():
     tickers = list(df.Symbol)
 
     return tickers
-
-tickers = get_sp500_tickers()
-
-
-
-
 
 def get_history(ticker, period_start, period_end, granularity="1d"):
 
@@ -65,9 +60,25 @@ def get_history(ticker, period_start, period_end, granularity="1d"):
 
 period_start = datetime(2010, 1, 1, tzinfo=pytz.utc)
 period_end = datetime(2020, 1, 1, tzinfo=pytz.utc)
+tickers = get_sp500_tickers()
 
+def get_histories(tickers, period_starts, period_ends, granularity="1d"):
+    dfs = [None]*len(tickers)
 
+    def _helper(i):
+        print(tickers[i])
+        df = get_history(
+            tickers[i],
+            period_starts[i],
+            period_ends[i],
+            granularity=granularity
+        )
+        dfs[i] = df
 
-for ticker in tickers:
-    df = get_history(ticker, period_start, period_end)
-    print(ticker, df)
+    threads = [threading.Thread(target=_helper,args=(i,)) for i in range(len(tickers))]
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+    return dfs
+
+dfs = get_histories(tickers, period_starts=[period_start]*len(tickers), period_ends=[period_end]*len(tickers))
+print(dfs)
